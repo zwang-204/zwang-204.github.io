b6fd882389f98935132c14c1c5fc230285ffcefd
GameTechArt-Tony <zitongwang0204@gmail.com>
2024-02-07 16:27:12 -0800

    firebase

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const counter = document.getElementById('counter');
-    const button = document.getElementById('counterButton');
+import { getDatabase, ref, set, child, get } from "firebase/database";
 
-    // Load the saved count from localStorage if it exists, or start with 0
-    let count = parseInt(localStorage.getItem('count')) || 0;
-    counter.innerText = count;
-
-    button.addEventListener('click', () => {
-        count += 1;
-        counter.innerText = count;
-        // Save the new count to localStorage
-        localStorage.setItem('count', count);
-    });
+const dbRef = ref(getDatabase());
+get(child(dbRef, `counts`)).then((counts) => {
+  if (counts.exists()) {
+    console.log(counts.val());
+  } else {
+    console.log("No data available");
+  }
+}).catch((error) => {
+  console.error(error);
 });
+
+// function readCounterAndUpdateUI() {
+//   const countRef = firebase.database().ref('counter/count');
+//   countRef.on('value', (snapshot) => {
+//     const count = snapshot.val();
+//     document.getElementById('counter').innerText = count;
+//   });
+// }
+
+// document.getElementById('counterButton').addEventListener('click', () => {
+//   const currentCount = parseInt(document.getElementById('counter').innerText);
+//   writeCounterData(currentCount + 1);
+// });
+
+// // Initial read to update UI
+// readCounterAndUpdateUI();
