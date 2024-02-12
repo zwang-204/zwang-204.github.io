a160613fc052c8b95f977c4b315ff0c7efdc240f
GameTechArt-Tony <zitongwang0204@gmail.com>
2024-02-11 19:52:56 -0800

    save the cat

--- src/index.js
+++ src/index.js
@@ -13,126 +13,57 @@ const firebaseApp = initializeApp({
 });
 
 const db = getDatabase();
-
-const redRef = ref(db, 'data/red');
-onValue(redRef, (_red) => {
-  const red = _red.val();
-  updateColor(0, red);
-});
-
-const greenRef = ref(db, 'data/green');
-onValue(greenRef, (_green) => {
-  const green = _green.val();
-  updateColor(1, green);
-});
-
-const blueRef = ref(db, 'data/blue');
-onValue(blueRef, (_blue) => {
-  const blue = _blue.val();
-  updateColor(2, blue);
-});
-
-let origR, origG, origB = 0;
-
-const compRef = ref(db, 'data/completed');
-onValue(compRef, (_comp) => {
-  const comp = _comp.val();
-  const completedText = document.getElementById("completedText");
-  completedText.textContent = `Match the left color. Completed: ${_comp.val()}`;
-  get(child(dbRef, `colorList/color/${comp+1}/` )).then((origColor) => {
-    if (origColor.exists()) {
-      origR = origColor.val().red;
-      origG = origColor.val().green;
-      origB = origColor.val().blue;
-      let colorBox = document.getElementById("colorBox1");
-      colorBox.style.backgroundColor = `rgb(${origR}, ${origG}, ${origB})`;
-    } else {
-      console.log("No data available");
-    }
-  }).catch((error) => {
-    console.error(error);
-  });
-});
-
 const dbRef = ref(db);
+let totalRescue = 0;
+
+// get(child(dbRef, `cat/total` )).then((total) => {
+//   if (total.exists()) {
+//     totalRescue = total.val();
+//   } else {
+//     console.log("No data available");
+//   }
+// }).catch((error) => {
+//   console.error(error);
+// });
+
+const totalCount = ref(db, 'cat/total');
+onValue(totalCount, (_total) => {
+  totalRescue = _total.val();
+})
+
+const saveCount = ref(db, 'cat/save');
+onValue(saveCount, (_save) => {
+  const save = _save.val();
+  updatePage(save);
+});
+
 
 document.addEventListener('DOMContentLoaded', () => {
-  
-  const redPlus = document.getElementById('redPlus');
-  const redMinus = document.getElementById('redMinus');
-  const greenPlus = document.getElementById('greenPlus');
-  const greenMinus = document.getElementById('greenMinus');
-  const bluePlus = document.getElementById('bluePlus');
-  const blueMinus = document.getElementById('blueMinus');
-  redPlus.addEventListener('click', () => {
-    writeData(0, 1);
+  const saveButton = document.getElementById('saveButton');
+  saveButton.addEventListener('click', () => {
+    writeData(1);
   });
-  redMinus.addEventListener('click', () => {
-    writeData(0, -1);
-  });
-  greenPlus.addEventListener('click', () => {
-    writeData(1, 1);
-  });
-  greenMinus.addEventListener('click', () => {
-    writeData(1, -1);
-  });
-  bluePlus.addEventListener('click', () => {
-    writeData(2, 1);
-  });
-  blueMinus.addEventListener('click', () => {
-    writeData(2, -1);
-  });
-
 });
 
-function updateColor(index, num) {
+function updatePage(numOfSaves) {
   // Find the HTML element by ID
-  const colorBox = document.getElementById("colorBox2");
-  var currentColor = window.getComputedStyle(colorBox).backgroundColor;
-  var [r, g, b] = currentColor.replace(/rgba?\((\d+), (\d+), (\d+).*\)/i, '$1,$2,$3').split(',').map(Number);
-  if(index == 0){
-    r = Math.min(Math.max(0, num), 255);
-    const redText = document.getElementById("redText");
-    redText.textContent = num;
-  }
-  else if(index == 1){
-    g = Math.min(Math.max(0, num), 255);
-    const greenText = document.getElementById("greenText");
-    greenText.textContent = num;
-  }
-  else if(index == 2){
-    b = Math.min(Math.max(0, num), 255);
-    const blueText = document.getElementById("blueText");
-    blueText.textContent = num;
-  }
+  const button = document.getElementById("saveButton");
+  const cat = document.getElementById("image1");
+  const dirt = document.getElementById("dirt");
+  button.textContent = `Save the cat\r\n`;
+  button.textContent += `${numOfSaves}/${totalRescue}`;
+  let percentage = numOfSaves / totalRescue;
+  let catPercent = 100 * ((1-percentage) * 0.85);
+  let dirtPercent = 100 * (percentage * 0.85);
 
-  checkColor(r, g, b);
-
-  colorBox.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+  cat.style.top = catPercent + "%";
+  dirt.style.height = dirtPercent + "%";
 }
 
-function checkColor(r, g, b){
-  if(r == origR && g == origG && b == origB){
-    origR = -1;
-    const dbRef = ref(getDatabase());
-    const updates = {};
-    updates[`data/completed`] = increment(1);
-    update(dbRef, updates);
-  }
-}
-
-function writeData(index, num){
+function writeData(num){
   const dbRef = ref(getDatabase());
   const updates = {};
-  if(index == 0){
-    updates[`data/red`] = increment(num);
-  }
-  else if(index == 1){
-    updates[`data/green`] = increment(num);
-  }
-  else if(index == 2){
-    updates[`data/blue`] = increment(num);
-  }
+  updates[`cat/save`] = increment(num);
   update(dbRef, updates);
 }
 
